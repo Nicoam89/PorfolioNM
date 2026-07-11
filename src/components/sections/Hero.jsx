2b90969646929c8ba77db4499c39ec888e49f900
Nicoam89 <nico.am98@gmail.com>
2026-07-11 10:48:23 -0300

    se modifica subtitulo hero

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
         <p className="hero-section__intro">Hola, mi nombre es</p>
         <h1 className="hero-section__title">Nicolas Ariel Martin</h1>
         <h2 className="hero-section__subtitle">Business & Functional Analyst</h2>
-        <h3 className="hero-section__subtitle2">Procesos | Requerimientos | Automatización | BI | Integración negocio–IT</h3>
+        <h3 className="hero-section__subtitle2">Procesos | Automatización | BI | Integración negocio–IT</h3>
         <p className="hero-section__description">
           Convierto necesidades de negocio en requerimientos claros, procesos medibles y soluciones tecnológicas que los equipos pueden implementar con foco en impacto.
         </p>
